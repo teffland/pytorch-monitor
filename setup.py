49e2d6cadf149a34d12d39d4f1c8487bf4463cb6
Thomas Effland <thomaseffland@Toms-MBP.lan>
2018-01-16 11:00:48 -0500

    newer version:

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 setup(
   name = 'pytorch-monitor',
   packages = ['pytorch_monitor'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   description = 'Monitor pytorch modules with minimal code.',
   author = 'Tom Effland',
   author_email = 'teffland@cs.columbia.edu',
   url = 'https://github.com/teffland/pytorch-monitor', # use the URL to the github repo
-  download_url = 'https://github.com/teffland/pytorch-monitor/archive/0.2.tar.gz',
+  download_url = 'https://github.com/teffland/pytorch-monitor/archive/0.3.tar.gz',
   keywords = ['pytorch', 'logging'], # arbitrary keywords
   classifiers = [],
   license='MIT',
