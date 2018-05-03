d65aafbcc593651bdce4f9ac5a96b91be27bceea
Thomas Effland <thomaseffland@toms-mbp-2.lan>
2018-05-03 11:37:32 -0400

    "auto commit tracked files for new experiment: <No Title on May-03-18@11:37:32"

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 setup(
   name = 'pytorch-monitor',
   packages = ['pytorch_monitor'], # this must be the same as the name above
-  version = '0.12',
+  version = '0.14',
   description = 'Monitor pytorch modules with minimal code.',
   author = 'Tom Effland',
   author_email = 'teffland@cs.columbia.edu',
   url = 'https://github.com/teffland/pytorch-monitor', # use the URL to the github repo
-  download_url = 'https://github.com/teffland/pytorch-monitor/archive/0.12.tar.gz',
+  download_url = 'https://github.com/teffland/pytorch-monitor/archive/0.14.tar.gz',
   keywords = ['pytorch', 'logging'], # arbitrary keywords
   classifiers = [],
   license='MIT',
